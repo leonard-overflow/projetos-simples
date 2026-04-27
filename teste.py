f41fc4664e96d6a67046d67949d81a1eebe4beca
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-04-27 19:40:35 -0300

    Criando mecanismo de reconhecimento de das teclas

--- teste.py
+++ teste.py
@@ -1,19 +1,7 @@
-import msvcrt
+import readchar
 
-print("Para qual direção você quer ir?: ", end="", flush=True)
-
-tecla = msvcrt.getch()
-
-if tecla == b'\xe0':  # seta especial
-    tecla = msvcrt.getch()
-    if tecla == b'H': direcao = "cima"
-    elif tecla == b'P': direcao = "baixo"
-    elif tecla == b'K': direcao = "esquerda"
-    elif tecla == b'M': direcao = "direita"
-elif tecla in (b'w', b'W'): direcao = "cima"
-elif tecla in (b's', b'S'): direcao = "baixo"
-elif tecla in (b'a', b'A'): direcao = "esquerda"
-elif tecla in (b'd', b'D'): direcao = "direita"
-else: direcao = "desconhecida"
-
-print(direcao)
+while True:
+    tecla = readchar.readkey()
+    print(repr(tecla))  # mostra os bytes reais, ex: '\xe0H'
+    if tecla == 'q':
+        break
